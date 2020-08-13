3a6b8ccfa6c5021d4ee226d37cd1333e67f032d7
godwill <godwill.makhubela@gmail.com>
2020-08-13 12:26:33 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { EntryDatailsComponent } from './entry-datails/entry-datails.component';
+import { EntryDetailComponent } from './entry-datails/entry-detail/entry-detail.component';
+import { EntryDetailListComponent } from './entry-datails/entry-detail-list/entry-detail-list.component';
+import { EntryDetailService } from './shared/entry-detail.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryDatailsComponent,
+    EntryDetailComponent,
+    EntryDetailListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [EntryDetailService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
